27782a2a87daf0f59d386054bd0546cab1d39642
YurySamakhvalau <dev@takemake.com>
2022-10-11 14:45:10 +0300

    Replace full page load event

--- src/utilities/ready.ts
+++ src/utilities/ready.ts
@@ -10,13 +10,13 @@ type documentReady = {
  * the function calls the passed function after 
  * the document has finished loading and the document has been parsed,
  * but sub-resources such as scripts, images, stylesheets and frames are still loading.
- * @param callback Callback
+ * @param {callback} () => {}
  * @returns void
  * @see https://developer.mozilla.org/en-US/docs/Web/API/Document/readyState#value
  * @see https://developer.mozilla.org/en-US/docs/Web/API/Document/DOMContentLoaded_event
+ * @example documentReady(() => {})
  */
 const documentReady: documentReady = callback => {
-
   if(document.readyState === 'loading') {
     document.addEventListener('DOMContentLoaded', callback, { once: true })
   } else {
@@ -32,19 +32,16 @@ type documentComplete = {
 /**
  * the function calls the passed function after 
  * the document and all sub-resources have finished loading. 
- * @param callback Callback
+ * @param {callback} () => {}
  * @returns void
  * @see https://developer.mozilla.org/en-US/docs/Web/API/Document/readyState#value
  * @see https://developer.mozilla.org/en-US/docs/Web/API/Document/readystatechange_event
+ * @see https://developer.mozilla.org/en-US/docs/Web/API/Window/load_event
+ * @example documentComplete(() => {})
  */
 const documentComplete: documentComplete = callback => {
-
   if(document.readyState !== 'complete') {
-    document.addEventListener('readystatechange', event => {
-      if(document.readyState === 'complete') {
-        callback()
-      }
-    })
+    window.addEventListener('load', callback, { once: true })
   } else {
     // call the passed function immediately
     callback()
--- src/utilities/selector-engine.ts
+++ src/utilities/selector-engine.ts
@@ -3,45 +3,57 @@
 type CSSSelector = string
 
 /**
- * 
- * @param selector CSSSelector
- * @param element HTMLElement
- * @returns a NodeList representing a list of elements matching the specified group of selectors
+ * Find all elements of the passed selectors of the element upon which it was called.
+ * @param {selectors} CSSSelector
+ * @param {element} HTMLElement
+ * @returns {Array} a NodeList representing a list of elements matching the specified group of selectors.
  * @see https://developer.mozilla.org/en-US/docs/Web/API/Element/querySelectorAll
+ * @example find<HTMLDivElement>('div, .div, #div')
+ * @example fint<HTMLSpanElement>('.selector', HTMLElement)
  */
 const find = <T extends HTMLElement = HTMLElement>(selectors: CSSSelector, element = document.documentElement): Array<T> => {
   return <Array<T>>Array.from(Element.prototype.querySelectorAll.call(element, selectors))
 }
 
 /**
- * 
- * @param selector CSSSelector
- * @param element HTMLElement
- * @returns the first descendant element of baseElement which matches the specified group of selectors or null, if didn't find
+ * Find the first element of from passed selectors of the element upon which it was called.
+ * @param {selectors} CSSSelector
+ * @param {element} HTMLElement
+ * @returns {HTMLElement|null} the first child element which matches the specified group of selectors or null, if didn't find.
  * @see https://developer.mozilla.org/en-US/docs/Web/API/Element/querySelector
+ * @example findOne<HTMLDivElement>('div, .div, #div')
+ * @example findOne<HTMLSpanElement>('.selector', HTMLElement)
  */
 const findOne = <T extends HTMLElement = HTMLElement>(selectors: string, element = document.documentElement): T | null => {
   return <T>Element.prototype.querySelector.call(element, selectors)
 }
 
 /**
- * 
- * @param element HTMLElement
- * @param selector CSSSelector
- * @returns 
+ * Find all of the child elements of the passed selectors of the element upon which it was called.
+ * @param {element} HTMLElement
+ * @param {selector} CSSSelector
+ * @returns {Array} a live HTMLCollection.
+ * @see https://developer.mozilla.org/en-US/docs/Web/API/Element/children
+ * @see https://developer.mozilla.org/en-US/docs/Web/API/Element/matches
+ * @example childrens<HTMLDivElement>(HTMLElement, '.childDiv')
+ * @example childrens<HTMLSpanElement>(HTMLElement, '.childSpan')
  */
 const childrens = <T extends HTMLElement = HTMLElement>(element: HTMLElement, selector: string): Array<T> => {
   return <Array<T>>Array.from(element.children).filter(child => child.matches(selector))
 }
 
 /**
- * 
- * @param element HTMLElement
- * @param selector CSSSelector
- * @returns 
+ * Find closest parent by selector.
+ * @param {element} HTMLElement
+ * @param {selector} CSSSelector
+ * @returns {HTMLElement|null} the first parent element that matches the selector or null, if didn't find.
+ * @see https://developer.mozilla.org/en-US/docs/Web/API/Node/parentElement
+ * @see https://developer.mozilla.org/en-US/docs/Web/API/Element/matches
+ * @example parents<HTMLDivElement>(HTMLElement, '.parentDiv')
+ * @example parents<HTMLSpanElement>(HTMLElement, '.parentSpan')
  */
-const parents = <T extends HTMLElement = HTMLElement>(element: HTMLElement, selector: string): T | null => {
-  let ancestor = element.parentElement
+const parents = <T extends HTMLElement = HTMLElement>(node: HTMLElement, selector: string): T | null => {
+  let ancestor = node.parentElement
 
     while (ancestor) {
       if (ancestor.matches(selector)) {
@@ -55,19 +67,26 @@ const parents = <T extends HTMLElement = HTMLElement>(element: HTMLElement, sele
 }
 
 /**
- * 
- * @param element HTMLElement
- * @returns 
+ * Find the element's parent.
+ * @param {element} HTMLElement
+ * @returns {HTMLElement|null} the first parent element or null, if didn't find.
+ * @see https://developer.mozilla.org/en-US/docs/Web/API/Node/parentElement
+ * @example parent<HTMLDivElement>(HTMLElement)
+ * @example parent<HTMLSpanElement>(HTMLElement)
  */
-const parent = <T extends HTMLElement = HTMLElement>(element: HTMLElement): T | null => {
-  return <T>element.parentElement
+const parent = <T extends HTMLElement = HTMLElement>(node: HTMLElement): T | null => {
+  return <T>node.parentElement
 }
 
 /**
- * 
- * @param element HTMLElement
- * @param selector CSSSelector
- * @returns 
+ * Find the previous Element.
+ * @param {element} HTMLElement
+ * @param {selector} CSSSelector
+ * @returns {HTMLElement|null} the element preceding the specified one in its parent's list of children, or null.
+ * @see https://developer.mozilla.org/en-US/docs/Web/API/Element/previousElementSibling
+ * @see https://developer.mozilla.org/en-US/docs/Web/API/Element/matches
+ * @example prev<HTMLDivElement>(HTMLElement, '.prevDiv')
+ * @example prev<HTMLSpanElement>(HTMLElement, '.prevSpan')
  */
 const prev = <T extends HTMLElement = HTMLElement>(element: HTMLElement, selector: string): T | null => {
   let previous = element.previousElementSibling
@@ -84,10 +103,14 @@ const prev = <T extends HTMLElement = HTMLElement>(element: HTMLElement, selecto
 }
 
 /**
- * 
- * @param element HTMLElement
- * @param selector CSSSelector
- * @returns 
+ * Find the next Element.
+ * @param {element} HTMLElement
+ * @param {selector} CSSSelector
+ * @returns {HTMLElement|null} the element immediately following the specified one in its parent's children list, or null.
+ * @see https://developer.mozilla.org/en-US/docs/Web/API/Element/nextElementSibling
+ * @see https://developer.mozilla.org/en-US/docs/Web/API/Element/matches
+ * @example next<HTMLDivElement>(HTMLElement, '.nextDiv')
+ * @example next<HTMLSpanElement>(HTMLElement, '.nextSpan')
  */
 const next = <T extends HTMLElement = HTMLElement>(element: HTMLElement, selector: string): T | null => {
   let next = element.nextElementSibling
@@ -107,12 +130,9 @@ const next = <T extends HTMLElement = HTMLElement>(element: HTMLElement, selecto
 export const selectorEngine = {
   find,
   findOne,
-
   childrens,
-
   parents,
   parent,
-  
   prev,
   next
 }
