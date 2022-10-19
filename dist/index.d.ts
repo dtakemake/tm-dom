a5396c6a0635c5e44a9d3f9301d15b967999fd70
Yury Samakhvalau <56564781+dtakemake@users.noreply.github.com>
2022-10-19 19:44:34 +0300

    Delete dist directory

--- dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-import { documentReady, documentComplete } from "./utilities/ready";
-import { selectorEngine } from "./utilities/selector-engine";
-export { documentReady, documentComplete, selectorEngine };
-//# sourceMappingURL=index.d.ts.map
